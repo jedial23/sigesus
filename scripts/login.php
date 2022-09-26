30271c6a9538f2aadfd410749e4a38577e41b808
jedial23 <jedial23@gmail.com>
2022-09-26 06:09:32 -0400

    Agregando los archivos inciales del proyecto

---
+++ scripts/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ingreso</title>
+    <link rel="stylesheet" href="css/estilos.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+    <link rel="script" href="scripts/login.php">
+    
+</head>
+<body>
+
+    <form action="login.php" method="post"></form>
+     <input type="text" name="email" placeholder="Ingrese su correo electrónico" id="">
+     <input type="password" name="password" id="" placeholder="Ingrese su contraseña">
+     <input type="button" value="Ingresar">
+</body>
+</html>
+
+
