5aeada7530ec92b07fb29bcfdfba5b3549797f92
Alejandro Flores Valdez <alejandro@mx-m45>
2022-10-11 14:57:22 -0400

    Modificando archivos para enlazar las hojas de estilos

--- php-login/index.php
+++ php-login/index.php
@@ -3,15 +3,15 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="php-login/assets/css/styles.css">
+	<link rel="stylesheet" href="">
 
 	<title>Bienvenidos</title>
 </head>
 <body>
 	<h1>Por favor ingrese o regístrese</h1>
 	<a href="login.php">Ingreso</a> o <a href="signup.php">Registro</a>
-	<?php
-		echo "Hola Mundo";
-	?>
+		<h1>Ingreso</h1>
+
+
 </body>
 </html>
--- php-login/login.php
+++ php-login/login.php
@@ -3,7 +3,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="php-login/assets/css/styles.css">
+	<link rel="stylesheet" href="css/styles.css">
 	<title>Ingreso</title>
 </head>
 <body>
