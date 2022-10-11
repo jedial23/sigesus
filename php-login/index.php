8e5b7e73322f2e49ba2a2051d2563857c55dfc89
Alejandro Flores Valdez <alejandro@mx-m45>
2022-10-11 04:28:09 -0400

    Modificando ficheros de estilos en rama borradores

--- php-login/index.php
+++ php-login/index.php
@@ -1,3 +1,17 @@
-<?php 
-	
- ?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="php-login/assets/css/styles.css">
+
+	<title>Bienvenidos</title>
+</head>
+<body>
+	<h1>Por favor ingrese o regístrese</h1>
+	<a href="login.php">Ingreso</a> o <a href="signup.php">Registro</a>
+	<?php
+		echo "Hola Mundo";
+	?>
+</body>
+</html>
